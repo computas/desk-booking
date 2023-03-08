439a98eb26c2f31b0073ac2a830eb1d51a58ccb7
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-03-08 17:01:36 +0100

    feat: Add connection to mongodb to main using environment variables

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
-from typing import Union
 from fastapi import FastAPI
+from pymongo import MongoClient
+import os
+
+ATLAS_URL = os.environ["ATLAS_URL"]
+DB_NAME = os.environ["DB_NAME"]
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(ATLAS_URL)
+    app.database = app.mongodb_client[DB_NAME]
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/test")
+async def get_sales():
+    sales = []
+    for sale in app.database["sales"].find({}):
+        sales.append(sale["customer"])
+    
+    return {"sales": sales}
