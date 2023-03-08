6d0497bdd2029875f9b316b249792ae7632f8dd9
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-03-08 16:08:49 +0100

    feat: Add connection to mongodb atlas

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
-from typing import Union
 from fastapi import FastAPI
+from pymongo import MongoClient
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URL"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/test")
+async def get_sales():
+    sales = []
+    for sale in app.database["sales"].find({}):
+        print(sale)
+    
+    # Return the retrieved documents as a JSON response
+    return {"sales": sales}
